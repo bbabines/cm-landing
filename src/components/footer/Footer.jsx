a6f097b11ab4df198f485a06b4241307b3b05d1e
unknown <bbabines@gmail.com>
2023-03-07 14:33:03 -0500

    Added icon class and Discord icon to footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
 import styles from "./Footer.module.css";
+import { FaDiscord } from "react-icons/fa";
 
 const Footer = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.nameContainer}>
-				<p className={styles.name}>Cohering Minds Logo/Discord/Social Media</p>
+				<p>
+					<FaDiscord className={styles.icons} />
+				</p>
 			</div>
 		</div>
 	);
